7eb31570ec0fa4a60c5826f7d7079f43d51e1381
Anton Todua <toduaanton@yandex.ru>
2016-05-16 20:07:23 +0300

    change filenames

--- scripts/test.py
+++ scripts/test.py
@@ -13,39 +13,39 @@ main_program_path = "../NamedEntityRecognition"
 crf_test_path = "../CRF++-0.58/crf_test" 
 crf_test_model_path = "../model.crf-model"
 
-def call_main_program( args, output_filename ):
-	with open( output_filename, 'wb' ) as file:
+def call_main_program( args, dst_filename ):
+	with open( dst_filename, 'wb' ) as file:
 		call( [main_program_path] + args, stdout=file )
 
-def save_file_in_cp1251( name ):
+def save_file_in_cp1251( src_filename, dst_filename ):
 	try:
-		text = open( name, 'r', encoding='utf-8' ).read()
+		text = open( src_filename, 'r', encoding='utf-8' ).read()
 		text_bytes = text.encode( 'cp1251', errors='replace' )
-		open( name + '.cp1251', 'wb' ).write( text_bytes )
+		open( dst_filename, 'wb' ).write( text_bytes )
 	except UnicodeDecodeError:
 		print( 'Error: ' + name )
 
-def stem_file( name ):
-	if path.isfile( name + '.json' ):
+def stem_file( src_filename, dst_filename ):
+	if path.isfile( dst_filename ):
 		return
 	call([mystem_path, mystem_flags, "--eng-gr", "-e", "cp1251",
-		"--format", "json", name, name + '.json' ])
+		"--format", "json", src_filename, dst_filename ])
 			
-def test_file( name ):
-	name = name
-	with open( name + '.crf-test', 'wb' ) as file:
-		call([crf_test_path, "-m", crf_test_model_path, name ], stdout=file )
+def test_file( src_filename, dst_filename ):
+	with open( dst_filename, 'wb' ) as file:
+		call([crf_test_path, "-m", crf_test_model_path, src_filename ], \
+			stdout=file )
 
 for ( dirpath, dirnames, filenames ) in walk( texts_path ):
 	for filename in filenames:
 		if filename.endswith( '.txt' ):
 			print( filename )
-			name = texts_path + filename
-			save_file_in_cp1251( name )
-			name += '.cp1251'
-			stem_file( name )
+			name = texts_path + filename[:-4]
+			save_file_in_cp1251( name + '.txt', name + '.cp1251' )
+			stem_file( name + '.cp1251', name + '.json' )
 			call_main_program( ['--prepare-test-file', name + '.json'], \
-				name + '.json.signs' )
-			test_file( name + '.json.signs' )
+				name + '.signs' )
+			test_file( name + '.signs', name + '.crf-tested' )
 			call_main_program( ['--prepare-answer-file', name + '.json', \
-				name + '.json.signs.crf-test'], name + '.answer' )
+				name + '.crf-tested'], name + '.task1' )
+
